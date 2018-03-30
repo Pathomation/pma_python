781be646196746259825e74abfc84b61767d0586
Emadeldeen-24 <37911596+Emadeldeen-24@users.noreply.github.com>
2018-03-30 10:19:13 +0200

    adding setup file

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='pma-python',
+      version='0.1',
+      description='Python wrapper library for PMA.start',
+      url='http://github.com/pathomation/pma-python',
+      author='Wim Waelput',
+      license='Pathomation',
+      packages=['pma-python'],
+      install_requires=[
+          'PIL', 'urllib', 'requests'
+      ],
+      zip_safe=False)
