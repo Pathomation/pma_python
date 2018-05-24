db2439470f28969954424375c1f2ee579fd14c3f
Oleg Kulaev <ovkulaev@gmail.com>
2018-05-24 14:44:00 +0300

    Fixes #1
    added long_desc.txt to package_data
    file reformatting
    added build\dist to .gitignore

--- setup.py
+++ setup.py
@@ -1,21 +1,26 @@
+import os
 from setuptools import setup
 
-with open('long_desc.txt') as file:
-    long_description = file.read()
-	  
+
+def read(file_name):
+    with open(os.path.join(os.path.dirname(__file__), file_name)) as f:
+        return f.read()
+
+
 setup(name='pma_python',
-      version='2.0.0.33',
+      version='2.0.0.34',
       description='Universal viewing of digital microscopy, whole slide imaging and digital pathology data',
-	  long_description=long_description,
+      long_description=read('long_desc.txt'),
       url='http://github.com/pathomation/pma_python',
       author='Pathomation',
-	  author_email='info@pathomation.com',
+      author_email='info@pathomation.com',
       license='http://free.pathomation.com/eula/',
       packages=['pma_python'],
-	  classifiers = [
-		'Development Status :: 3 - Alpha', 
-		'Programming Language :: Python :: 3'],
-	  keywords='wsi whole slide imaging gigapixel microscopy histology pathology',
-	  install_requires=['pillow', 'requests'],
-	  python_requires='>=3',	# assume this only works in Python 3
+      data_files=[('', ['long_desc.txt'])],
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Programming Language :: Python :: 3'],
+      keywords='wsi whole slide imaging gigapixel microscopy histology pathology',
+      install_requires=['pillow', 'requests'],
+      python_requires='>=3',  # assume this only works in Python 3
       zip_safe=False)
