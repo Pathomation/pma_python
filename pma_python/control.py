248ca1eb1c661f28756f9ce45159ebcc0cd1e079
Yves Sucaet <37899889+YvesSucaet@users.noreply.github.com>
2018-06-28 04:49:11 -0400

    Refactoring of some common functionality back into a general pma module

--- pma_python/control.py
+++ pma_python/control.py
@@ -1,4 +1,20 @@
 import os
 import requests
+from pma_python import core, pma
 
-__version__ = "2.0.0.45"
+__version__ = pma.__version__
+
+def get_version_info(pmacontrolURL):
+	"""
+	Get version info from PMA.control instance running at pmacontrolURL
+	"""
+	# why? because GetVersionInfo can be invoked WITHOUT a valid SessionID; _pma_api_url() takes session information into account
+	url = pma._pma_join(pmacontrolURL, "api/version")
+	try:
+		headers = {'Accept': 'application/json'}
+		r = requests.get(url, headers=headers)
+	except Exception as e:
+		return None		
+	return r.json()
+
+	
