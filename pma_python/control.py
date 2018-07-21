42bf10cd824363d29fc0c1e49521a0e1428dca9c
Yves Sucaet <37899889+YvesSucaet@users.noreply.github.com>
2018-07-21 04:09:47 -0400

    Added comments; removed stub for get_session_participants

--- pma_python/control.py
+++ pma_python/control.py
@@ -18,6 +18,9 @@ def get_version_info(pmacontrolURL):
 	return r.json()
 
 def get_sessions(pmacontrolURL, pmacoreSessionID):
+	"""
+	Retrieve a list of currently defined training sessions in PMA.control.
+	"""
 	url = pma._pma_join(pmacontrolURL, "api/Sessions?sessionID=" + pma._pma_q(pmacoreSessionID))
 	print (url)
 	try:
@@ -28,6 +31,12 @@ def get_sessions(pmacontrolURL, pmacoreSessionID):
 	return r.json()
 
 def get_session_ids(pmacontrolURL, pmacoreSessionID):
+	"""
+	Retrieve a dictionary with currently defined training sessions in PMA.control.
+	The resulting dictionary use the session's identifier as the dictionary key, and 
+	therefore this method is easier to quickly retrieve and represent session-related data. 
+	However, this method returns less verbose data than get_sessions()
+	"""
 	full_sessions = get_sessions(pmacontrolURL, pmacoreSessionID)
 	new_session_dict = {}
 	for sess in full_sessions:
@@ -41,10 +50,10 @@ def get_session_ids(pmacontrolURL, pmacoreSessionID):
 		new_session_dict[sess["Id"]] = sess_data
 	return new_session_dict
 	
-def get_session_participants(pmacontrolURL, pmacoreSessionID, session):
-	return None
-	
 def get_case_collections(pmacontrolURL, pmacoreSessionID):
+	"""
+	Retrieve all the data for all the defined case collections in PMA.control
+	"""
 	url = pma._pma_join(pmacontrolURL, "api/CaseCollections?sessionID=" + pma._pma_q(pmacoreSessionID))
 	print (url)
 	try:
@@ -55,6 +64,9 @@ def get_case_collections(pmacontrolURL, pmacoreSessionID):
 	return r.json()
 	
 def get_projects(pmacontrolURL, pmacoreSessionID):
+	"""
+	Retrieve all projects and their data in PMA.control
+	"""
 	url = pma._pma_join(pmacontrolURL, "api/Projects?sessionID=" + pma._pma_q(pmacoreSessionID))
 	print (url)
 	try:
