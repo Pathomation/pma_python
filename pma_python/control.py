84492b46baa41bf99cadbabec672fc77f57d222c
Yves Sucaet <37899889+YvesSucaet@users.noreply.github.com>
2018-07-21 03:55:48 -0400

    Added new methods to interact w/ PMA.control

--- pma_python/control.py
+++ pma_python/control.py
@@ -17,4 +17,49 @@ def get_version_info(pmacontrolURL):
 		return None		
 	return r.json()
 
+def get_sessions(pmacontrolURL, pmacoreSessionID):
+	url = pma._pma_join(pmacontrolURL, "api/Sessions?sessionID=" + pma._pma_q(pmacoreSessionID))
+	print (url)
+	try:
+		headers = {'Accept': 'application/json'}
+		r = requests.get(url, headers=headers)
+	except Exception as e:
+		return None		
+	return r.json()
+
+def get_session_ids(pmacontrolURL, pmacoreSessionID):
+	full_sessions = get_sessions(pmacontrolURL, pmacoreSessionID)
+	new_session_dict = {}
+	for sess in full_sessions:
+		sess_data = {
+			"LogoPath": sess["LogoPath"],
+			"StartsOn": sess["StartsOn"],
+			"EndsOn": sess["EndsOn"],
+			"ModuleId": sess["ModuleId"],
+			"State": sess["State"]
+		}
+		new_session_dict[sess["Id"]] = sess_data
+	return new_session_dict
 	
+def get_session_participants(pmacontrolURL, pmacoreSessionID, session):
+	return None
+	
+def get_case_collections(pmacontrolURL, pmacoreSessionID):
+	url = pma._pma_join(pmacontrolURL, "api/CaseCollections?sessionID=" + pma._pma_q(pmacoreSessionID))
+	print (url)
+	try:
+		headers = {'Accept': 'application/json'}
+		r = requests.get(url, headers=headers)
+	except Exception as e:
+		return None		
+	return r.json()
+	
+def get_projects(pmacontrolURL, pmacoreSessionID):
+	url = pma._pma_join(pmacontrolURL, "api/Projects?sessionID=" + pma._pma_q(pmacoreSessionID))
+	print (url)
+	try:
+		headers = {'Accept': 'application/json'}
+		r = requests.get(url, headers=headers)
+	except Exception as e:
+		return None		
+	return r.json()
