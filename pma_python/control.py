3116dcb0f5f502badc9eba0bc052630ce34a0897
Yves Sucaet <37899889+YvesSucaet@users.noreply.github.com>
2019-04-23 14:35:17 +0200

    fixes wrt/ participant registration

--- pma_python/control.py
+++ pma_python/control.py
@@ -150,6 +150,7 @@ def register_participant_for_training_session(pmacontrolURL, participantUsername
 	if is_participant_in_training_session(pmacontrolURL, participantUsername, pmacontrolSessionID, pmacoreSessionID):
 		raise NameError ("PMA.core user " + participantUsername + " is ALREADY registered in PMA.control training session " + str(pmacontrolSessionID))
 	url = pma._pma_join(pmacontrolURL, "api/Sessions/") + str(pmacontrolSessionID) + "/AddParticipant?SessionID=" + pmacoreSessionID
+	print(url)
 	data = { "UserName": participantUsername, "Role": pmacontrolRole}   # default interaction mode = Locked
 	data = parse.urlencode(data).encode()
 	req =  request.Request(url=url, data=data) # this makes the method "POST"
