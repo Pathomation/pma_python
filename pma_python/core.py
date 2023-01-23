27b0c74d1c324f8c70eb25d53a651e12062fd276
Mehdi Sahri <114163992+mehdipma@users.noreply.github.com>
2023-01-23 13:02:59 +0100

    test_commit_2

--- pma_python/core.py
+++ pma_python/core.py
@@ -1684,4 +1684,3 @@ def get_annotation_distance(slideRef, layerID, annotationID, sessionID=None):
         raise Exception("get_annotation_distance on  " + slideRef + " resulted in error")
 
     return r.text
-
